1f99c78ae853bd44b0ab32dd7eefddb6301ddf30
Kozlovska Yuliia <kozlovskayayulya2@gmail.com>
2023-08-04 18:35:15 +0300

    jsx config

---
+++ todo-list/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const root = document.getElementById('root');
+
+const element = <h1 class="title">Todo List</h1>;
+
+ReactDOM.render(element, root);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
